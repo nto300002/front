fb74eebd7f5d037961de3060116251f98d28771f
nto300002 <samonkntd@gmail.com>
2024-01-05 20:13:23 +0900

    add: footer

---
+++ app/components/layout/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <div className="bg-red-700 text-white text-center">
+      © 2023 yasuda naoto Inc.
+    </div>
+  );
+};
+
+export default Footer;
