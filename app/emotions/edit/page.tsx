f37716411b742e6945a5840f36de89e488e8a6fe
nto300002 <samonkntd@gmail.com>
2024-01-08 00:40:34 +0900

    add: emotions-edit-form page UI

---
+++ app/emotions/edit/page.tsx
@@ -0,0 +1,13 @@
+import EmotionFormEdit from '@/app/components/emotions/EmotionFormEdit';
+import MainContent from '@/app/components/layout/MainContent';
+import React from 'react';
+
+const EmotionsEditPost = () => {
+  return (
+    <MainContent>
+      <EmotionFormEdit />
+    </MainContent>
+  );
+};
+
+export default EmotionsEditPost;
